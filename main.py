75dfed41d32ef13a41501d8c033b7d49d0996abf
Neil Hemmerly <neilhemm0@gmail.com>
2024-12-15 21:09:40 -0500

    create asteroid field and movement

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import pygame
+from circleshape import CircleShape
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
+    player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for ent in updateable:
+            ent.update(dt)
+
         screen.fill((0,0,0))
+
+        for ent in drawable:
+            ent.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
